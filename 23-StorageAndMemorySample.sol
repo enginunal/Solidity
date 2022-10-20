da29d1e420e6ed326f713361b6dbee38d5462bce
Engin ÜNAL <enginunal@gmail.com>
2022-10-20 23:23:33 +0300

    Update 23-StorageAndMemorySample.sol

--- 23-StorageAndMemorySample.sol
+++ 23-StorageAndMemorySample.sol
@@ -7,16 +7,19 @@ contract MemSample {
         bool st_Y;
     }
     
-    uint256    s_a;  //storage 
-    string     s_b;  //storage 
-    uint256[2] s_c;  //storage 
-    MyStruct   s_d;  //storage
-    
-    function Sample() public pure {
-        uint256           m_a;  //memory
-        string     memory m_b;  //memory
-        uint256[2] memory m_c;  //memory
-        MyStruct   memory m_d;  //memory
+    uint256               s_a;  //storage 
+    string                s_b;  //storage 
+    uint256[2]            s_c;  //storage 
+    MyStruct              s_d;  //storage
+    mapping(uint => uint) s_e;  //storage
+    string                s_f;  //storage
 
+    function Sample() public pure {
+        uint256              m_a;  //memory
+        string        memory m_b;  //memory
+        uint256[2]    memory m_c;  //memory
+        MyStruct      memory m_d;  //memory
+        //mapping tanımı memory olarak yapılamaz! Sadece storage tanımlanır.
+        string        memory m_f;  //memory
     }
 }
