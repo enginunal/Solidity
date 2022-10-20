534af03452c2dbcd03ce961a738447ceb478618e
Engin ÜNAL <enginunal@gmail.com>
2022-10-20 23:03:47 +0300

    Create StorageAndMemorySample.sol

---
+++ StorageAndMemorySample.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.0;
+
+contract MemSample {
+    struct MyStruct {
+        int  st_X;
+        bool st_Y;
+    }
+    
+    uint256    s_a;  //storage 
+    string     s_b;  //storage 
+    uint256[2] s_c;  //storage 
+    MyStruct   s_d;  //storage
+    
+    function Sample() public pure {
+        uint256           m_a;  //memory
+        string     memory m_b;  //memory
+        uint256[2] memory m_c;  //memory
+        MyStruct   memory m_d;  //memory
+
+    }
+}
